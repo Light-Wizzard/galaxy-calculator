6111c6419b92c84792c78472354bd3e3f63f5abd
Light-Wizzard <thelightwizzard@gmail.com>
2020-04-18 16:34:57 -0700

    Update

--- tools/build-with-qmake.sh
+++ tools/build-with-qmake.sh
@@ -34,38 +34,38 @@ declare NC='\033[0m';
 declare QT_BEINERI_VERSION; QT_BEINERI_VERSION="$1";
 echo "$QT_BEINERI_VERSION";
 # Qt Version to install based on travis.yml Environment Variable QT_BEINERI_VERSION
-if [ -z "$QT_BEINERI_VERSION" ]; then 
+if [ -z "${QT_BEINERI_VERSION+x}" ]; then 
     echo -e "${WARNING_COLOR}Add QT_BEINERI_VERSION to call this script in your travis.yml file to use from beineri repo, qt512${NC}";
     QT_BEINERI_VERSION="514";
     if [ "${EXIT_ON_UNDEFINED}" -eq 1 ]; then exit 1; fi    
 fi
 # QTV You can set this in your Environment, so do not overwrite it
-if [ -z "${QTV}" ]; then
+if [ -z "${QTV+x}" ]; then
     declare -x QTV; QTV="$QT_BEINERI_VERSION";
 fi
 # Define GITHUB_TOKEN in your Travis Settings Environment Variable error if not set, its not safe to store it in plain text
-if [ -z "${GITHUB_TOKEN}" ]; then
+if [ -z "${GITHUB_TOKEN+x}" ]; then
     echo -e "${WARNING_COLOR}Add GITHUB_TOKEN to your Travis Settings Environment Variable with a value from Github Settings Developer Personal access tolkens${NC}";
     if [ "${EXIT_ON_UNDEFINED}" -eq 1 ]; then exit 1; fi    
 fi
 # Define GITHUB_EMAIL in your Travis Settings Environment Variable error if not set, its not safe to store it in plain text
-if [ -z "${GITHUB_EMAIL}" ]; then
+if [ -z "${GITHUB_EMAIL+x}" ]; then
     echo -e "${WARNING_COLOR}Add GITHUB_EMAIL to your Travis Settings Environment Variable with your Github email address${NC}";
     if [ "${EXIT_ON_UNDEFINED}" -eq 1 ]; then exit 1; fi    
 fi
 # If not defined it will use this as a default
-if [ -z "${BIN_PRO_RES_NAME}" ]; then
+if [ -z "${BIN_PRO_RES_NAME+x}" ]; then
     echo -e "${WARNING_COLOR}Add BIN_PRO_RES_NAME (BIN_PRO_RES_NAME=${BIN_PRO_RES_NAME}) to your Travis Settings Environment Variable with a value from Github value for Binary, pro, and resource Name ${NC}";
     if [ "${EXIT_ON_UNDEFINED}" -eq 1 ]; then exit 1; fi    
 fi
 # TRAVIS_REPO_SLUG should always have your GITHUB_USERNAME as the first part / GITHUB_PROJECT, so I split them to use later.
-if [ -z "${GITHUB_USERNAME}" ] || [ -z "${GITHUB_PROJECT}" ]; then
+if [ -z "${GITHUB_USERNAME+x}" ] || [ -z "${GITHUB_PROJECT}" ]; then
     OLD_IFS="$IFS"; IFS='/'; read -ra repo_parts <<< "$TRAVIS_REPO_SLUG"; IFS="$OLD_IFS";
     declare -x GITHUB_PROJECT;
     GITHUB_USERNAME="${repo_parts[0]}";  GITHUB_PROJECT="${repo_parts[1]}";
 fi
 # Qt Installer Framework Package Folder
-if [ -z "${QIF_PACKAGE_URI}" ]; then
+if [ -z "${QIF_PACKAGE_URI+x}" ]; then
     echo -e "${WARNING_COLOR}Add QIF_PACKAGE_URI to your Travis Settings Environment Variable with the URI for your Project.${NC}";
     if [ "${EXIT_ON_UNDEFINED}" -eq 1 ]; then exit 1; fi    
 fi
@@ -75,7 +75,7 @@ if [ -z "${QIF_PACKAGE_DATA}" ]; then
 fi
 # I downloaded the version of Qt Installer I needed, and 7ziped the bin folder
 # I put it in the tools folder, I will extract it later
-if [ -z "${QIF_ARCHIVE}" ]; then
+if [ -z "${QIF_ARCHIVE+x}" ]; then
     echo -e "${WARNING_COLOR}Add QIF_ARCHIVE to your Travis Settings Environment Variable with the folder/file.7z that contains an Archive of the Qt Installer Framework bin folder${NC}";
     declare -x QIF_ARCHIVE; QIF_ARCHIVE="tools/qtinstallerframework.7z";
 fi
